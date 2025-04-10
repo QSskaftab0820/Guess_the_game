6d1b8d88e5616b67252828cf1913edf2d76e062c
SK AFTAB <akaftabsqalbi0820@gmail.com>
2025-04-10 17:00:10 +0530

    Update image_predictor.py

--- TKINTER/image_predictor.py
+++ TKINTER/image_predictor.py
@@ -4,7 +4,7 @@ import random
 # Word-to-image mapping (you can customize or expand this)
 image_dict = {
     "Dog": "https://th.bing.com/th/id/OIP.sweV5y8OvabQrCD2IMdYJgHaFj?rs=1&pid=ImgDetMain",
-    "Cat": "https://th.bing.com/th/id/OIP.TzP2op3lkhlTh6oOHamacAHaHa?w=249&h=249&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    "Cat": "https://th.bing.com/th/id/OIP.bqgfufo9kLtOXC05vNmfagAAAA?rs=1&pid=ImgDetMain",
     "Parrot": "https://img.freepik.com/premium-photo/colorful-parrot-sitting-branch-forest_871710-24980.jpg",
     "Horse": "https://th.bing.com/th/id/OIP.Xw9TPYUwQnVXJEfK4CVm4AHaJQ?w=1384&h=1730&rs=1&pid=ImgDetMain"
 }
